89218fd71507688ead2fcd32b657bed19d9b4e6f
Travis Treseder <travis.treseder@ldschurch.org>
2014-01-31 12:53:43 -0700

    Remove cruft funtions

--- static/model.js
+++ static/model.js
@@ -21,10 +21,9 @@ function EpisodeIV(db) {
   self.getBooks = function() {
       return ["Gen", "Exod", "Lev", "Num", "Deut", "Josh", "Judg", "Ruth", "1Sam", "2Sam", "1Kgs", "2Kgs", "1Chr", "2Chr", "Ezra", "Neh", "Esth", "Job", "Ps", "Prov", "Eccl", "Song", "Isa", "Jer", "Lam", "Ezek", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nah", "Hab", "Zeph", "Hag", "Zech", "Mal", "Matt", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phil", "Col", "1Thess", "2Thess", "1Tim", "2Tim", "Titus", "Phlm", "Heb", "Jas", "1Pet", "2Pet", "1John", "2John", "3John", "Jude", "Rev"];
   };
-  self.getChapters = function() {}
 
   self.getChapterForBook = function(book) {
-      BOOK_DATA[book]["chapters"].length
+      return BOOK_DATA[book]["chapters"].length
   };
 
   self.settings.set = function(setting, value) {
